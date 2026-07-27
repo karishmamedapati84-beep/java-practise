8e9839cae4922fd7a67ef2afec0f2aa50ec8da1d
M.Karishma <karishmamedapati84@gmail.com>
2026-07-27 17:49:57 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,5 @@
+class FirstProgram{
+    public static void main(String[] args) { //The main() method is the entry point into the application 
+        System.out.println("Hello World!");       
+}
+}
