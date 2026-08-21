9046c4ae17f15db7401b2bc5b14d50d112b87393
M.Karishma <karishmamedapati84@gmail.com>
2026-08-21 11:15:11 +0530

    Add files via upload

---
+++ ReplaceMethod.java
@@ -0,0 +1,9 @@
+//wap to replace spaces with underscores
+class ReplaceMethod {
+    public static void main(String args[]){
+        String a="Hello i am Karishma";
+        System.out.println(a.replace(" ", "_"));
+    }
+
+}    
+
---
+++ StringExample1.java
@@ -0,0 +1,7 @@
+//Wap to convert a string to lower case
+class StringExample1 {
+    public static void main(String args[]){
+        String name="KARISHMA";
+        System.out.println(name.toLowerCase());
+    }
+}
