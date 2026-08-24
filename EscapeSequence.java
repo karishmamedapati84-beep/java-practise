2872f0070936dccb59e0d1c8f28c4d5b8e18424f
M.Karishma <karishmamedapati84@gmail.com>
2026-08-24 16:48:56 +0530

    Add files via upload

---
+++ EscapeSequence.java
@@ -0,0 +1,7 @@
+//Wap to format the following letter using escape sequence characters.Letter="Hello Karishma! Welcome to java course."
+class EscapeSequence {
+    public static void main(String args[]){
+        String mystring="Hello Karishma!\n\t Welcome to Java Course.";
+        System.out.println(mystring);
+    }
+}
