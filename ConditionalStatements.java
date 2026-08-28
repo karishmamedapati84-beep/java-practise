4c3042c74e7359b03212a3fa3aa6ef4e22417b76
M.Karishma <karishmamedapati84@gmail.com>
2026-08-28 20:55:25 +0530

    Add files via upload

---
+++ ConditionalStatements.java
@@ -0,0 +1,23 @@
+class ConditionalStatements {
+    public static void main(String args[]){
+        /* 
+        int age=27;
+        if(age>18){
+            System.out.println("Yes You can drive the bike!");
+        }
+        else{
+            System.out.println("No you cannot drive the bike yet!");
+        }
+            */
+
+        int age=19;
+        boolean cond=(age==18);
+        if(cond){
+            System.out.println("Yes You can drive the bike!");
+        }
+        else{
+            System.out.println("No you cannot drive the bike yet!");
+        }
+
+    }  
+}
