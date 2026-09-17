d2898ddd06ebe175b358beba49113cac6c89cd28
M.Karishma <karishmamedapati84@gmail.com>
2026-09-17 21:48:11 +0530

    Add files via upload

---
+++ NaturalNumbers.java
@@ -0,0 +1,10 @@
+//WAP to print natural numbers from 100 to 200
+class NaturalNumbers{
+    public static void main(String args[]){
+        int i=100;
+        while(i<=200){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
