f66bbe90a8a2505264ae1f6d1b975090bd248fae
M.Karishma <karishmamedapati84@gmail.com>
2026-09-19 22:07:08 +0530

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,12 @@
+class WhileLoop{
+    public static void main(String args[]){
+        System.out.println("While loop");   
+        int i=1;
+        while(i<=3){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finish running while loop!");
+        
+    }
+}
