ecc8b13d3cbac1b1add1024456277e8993abe459
M.Karishma <karishmamedapati84@gmail.com>
2026-09-21 09:40:15 +0530

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,17 @@
+class DoWhileLoop {
+    public static void main(String args[]){
+    //    int b=0;
+    //    do{
+    //     System.out.println(b);
+    //     b++;
+    //    }
+    //    while(b<5);
+    /* WAP to print first n-natural numbers using do while loop */
+         int n=1;
+         do{
+            System.out.println(n);
+            n++;
+         }while(n<=500);
+    }    
+}
+
