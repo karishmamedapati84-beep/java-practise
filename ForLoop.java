bed6ea061730734188223765140f6946aaadc74b
M.Karishma <karishmamedapati84@gmail.com>
2026-09-22 09:53:49 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,13 @@
+class ForLoop {
+    public static void main(String args[]){
+        // for(int i=1;i<=10;i++){
+        //     System.out.println(i);
+        // }
+
+
+        //decreament
+        for(int i=10;i>0;i--){
+            System.out.println(i);
+        }
+    }    
+}
