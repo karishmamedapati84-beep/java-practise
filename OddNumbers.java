6c3391aa0502154ea8bda93d09b495706be76bc8
M.Karishma <karishmamedapati84@gmail.com>
2026-09-23 14:22:46 +0530

    Add files via upload

---
+++ OddNumbers.java
@@ -0,0 +1,9 @@
+//WAP to print first n odd numbers using a for loop
+class OddNumbers {
+    public static void main(String args[]){
+        int n=20;
+        for(int i=1;i<=n;i++){
+            System.out.println(2*i+1);
+        }
+    }
+}
